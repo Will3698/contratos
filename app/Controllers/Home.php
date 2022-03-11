f9b1a4bdb0f5cbcaa36332f624fd1ee640c1bf43
Rafael Martins <88293221+rafaelmartins17@users.noreply.github.com>
2022-03-11 17:41:44 -0300

    Atualizações no Front-End #18
    Implementação dos registros de data de pagamento dos contratos #18

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,66 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
+
 class Home extends BaseController
 {
     public function index()
     {
-        return view('pagina_inicial_view');
+        //$arr['arr'] = null;
+        $url = "http://localhost:8080/api/pagamento";
+        $arr['list'] = json_decode(file_get_contents($url), true);
+        //print_r($arr['list'][0]['id_contrato_pag']['nome']);
+        //die();
+        return view('pagina_inicial_view', $arr);
     }   
+
+    public function getDados(){
+        $url = "http://localhost:8080/api/contrato/pago";
+        $arr = json_decode(file_get_contents($url), true);
+
+        //print($arr);
+        //die();
+        $cols = [];
+        $rows = [];
+        $dados = [];
+
+        $cols = [
+            [
+                'id' => '',
+                'label' => 'Mês',
+                'pattern' => '',
+                'type' => 'string'
+            ],
+            [
+                'id' => '',
+                'label' => 'Valor Pago',
+                'pattern' => '',
+                'type' => 'number'   
+            ]
+        ];
+
+        foreach($arr as $arr){
+            $rows[] = [
+                'c' => [
+                    [
+                        'v' => $arr['nome'],
+                        'f' => null
+                    ],
+                    [
+                        'v' => (int) $arr['valor_pagar'],
+                        'f' => null
+                    ]
+                ]
+            ];
+        }
+
+        $dados = [
+            'cols' => $cols,
+            'rows' => $rows
+        ];
+
+        //echo '<pre>';
+        echo json_encode($dados);        
+    }
 }
--- app/Views/pagina_inicial_view.php
+++ app/Views/pagina_inicial_view.php
@@ -13,38 +13,35 @@
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="<?= base_url("css/styles.css") ?>" rel="stylesheet" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
+
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script>
         //carregar a API de visualizacao e o pacote do gráfico
         google.charts.load('current', {
-            'packages' : [
+            'packages': [
                 'corechart'
             ]
         });
 
         google.charts.setOnLoadCallback(drawChart);
 
-        function drawChart(){
-            var jsonData = $.ajax(
-                {
-                    url :  'contratocontroller/getDados',
-                    dataType: 'json',
-                    async: false
-                }
-            ).responseText
+        function drawChart() {
+            var jsonData = $.ajax({
+                url: 'home/getDados',
+                dataType: 'json',
+                async: false
+            }).responseText
 
             var data = new google.visualization.DataTable(jsonData);
             var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
 
             chart.draw(
-                data,
-                {
+                data, {
                     width: '100%',
-                    height: 400
+                    height: 200
                 }
             );
         }
@@ -71,7 +68,7 @@
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                            <li class="nav-item active"><a class="nav-link" href="#!">Home</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="<?= site_url("home/index") ?>">Home</a></li>
                             <li class="nav-item"><a class="nav-link" href="#!">Link</a></li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
@@ -88,8 +85,32 @@
             </nav>
             <!-- Page content-->
             <div class="container-fluid">
-                <b>RELATÓRIO DE PAGAMENTOS</b>
-                <div id="chart_div"></div>
+                <div class="form-group row">
+                    <b>RELATÓRIO DE PAGAMENTOS</b>
+                    <div id="chart_div"></div>
+
+                    <hr>
+
+                    <br>
+                    <table class="table table-light table-hover" style="text-align: center;">
+                        <thead class="table-secondary">
+                            <tr>
+                                <th scope="col">Nome da Empresa</th>
+                                <th scope="col">Valor Pago</th>
+                                <th scope="col">Data de Pagamento</th>                                
+                            </tr>
+                        </thead>
+                        <?php foreach ($list as $inf) : ?>
+                            <tbody>
+                                <tr>                                    
+                                    <td><?php print $inf['id_contrato_pag']['nome']?></td>
+                                    <td><?php print $inf['id_contrato_pag']["valor_pagar"] ?></td>
+                                    <td><?php print date("d-m-Y", strtotime($inf["registro"])) ?></td>                                    
+                                </tr>
+                            </tbody>
+                        <?php endforeach; ?>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
